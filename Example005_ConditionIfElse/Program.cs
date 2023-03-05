77872dc9b37257f13197740c25fffa403df37ead
Uneld <aleksey.minchenya@gmail.com>
2023-03-05 12:53:46 +0300

    added new project 05

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter your name");
+string name = Console.ReadLine();
+
+if (name == "Alex"){
+    Console.WriteLine("EEE it`s Alex"); 
+} else {
+    Console.WriteLine("Hellow, ");
+    Console.WriteLine(name);
+}
